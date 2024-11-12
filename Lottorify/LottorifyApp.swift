420d49ca851559b131e05e8f95a26957a1fbebe5
kevinumarov <keulifeinfluencer@gmail.com>
2024-11-13 00:34:05 +0900

    Initial Commit

---
+++ Lottorify/LottorifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottorifyApp.swift
+//  Lottorify
+//
+//  Created by Kevin Umarov on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct LottorifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
